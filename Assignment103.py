9d80bcae91f0188fb08e5cc0fd880219468644cb
SrikanthMaku <136585124+SrikanthMaku@users.noreply.github.com>
2023-09-06 21:56:29 +0530

    Add files via upload

---
+++ Assignment103.py
@@ -0,0 +1,15 @@
+# Write a Python program to count the number of even and odd numbers from a series of numbers.
+
+list = [1,2,3,4,5,6,7,8,9]
+
+even_count = 0
+odd_count = 0
+
+for i in list:
+    if i % 2==0:
+        even_count += 1
+    else:
+        odd_count += 1                  
+
+print("No of even numbers:",even_count)
+print("No of odd numbers:",odd_count)
